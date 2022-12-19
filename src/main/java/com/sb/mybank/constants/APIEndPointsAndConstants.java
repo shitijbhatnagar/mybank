132881401a07536e5aa9bc092b9264227ff5d5b1
Shitij Bhatnagar <shitijbhatnagar@yahoo.com>
2022-12-19 20:41:16 +0530

    Updates for initial Wire Mock changes as of 19-Dec-2022 - v2

--- src/main/java/com/sb/mybank/constants/APIEndPointsAndConstants.java
+++ src/main/java/com/sb/mybank/constants/APIEndPointsAndConstants.java
@@ -4,7 +4,8 @@ public interface APIEndPointsAndConstants
 {
     public static String api_getTransactions = "/transactions";
     public static String api_createTransaction = "/transactions";
+    public static String uuidBaseValue = "dc82a225-bfff-4bb7-b13b-cdba279a60b7";
 
-    String uuidBaseValue = "dc82a225-bfff-4bb7-b13b-cdba279a60b7";
+    public static int wireMockPort = 8090;
 
 }
--- src/test/java/com/sb/mybank/web/AccountTransactionControllerIT.java
+++ src/test/java/com/sb/mybank/web/AccountTransactionControllerIT.java
@@ -3,29 +3,17 @@ package com.sb.mybank.web;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.tomakehurst.wiremock.WireMockServer;
-import com.github.tomakehurst.wiremock.client.WireMock;
-import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
-import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import com.sb.mybank.config.TestConfig;
 import com.sb.mybank.constants.APIEndPointsAndConstants;
 import com.sb.mybank.util.MockDataProvider;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Rule;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import okhttp3.*;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.junit.Assert.assertEquals;
@@ -33,14 +21,10 @@ import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options
 
 @Slf4j
 @Import(TestConfig.class)
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @TestPropertySource("classpath:test-application.properties")
 public class AccountTransactionControllerIT
 {
-//    @Rule
-//    public WireMockRule wireMockRule=new WireMockRule(options().dynamicPort().dynamicPort());
-
-    static WireMockServer wireMockRule;
+    static WireMockServer wireMockServer;
 
     String mockTransactionJSON = "[{\\\"id\":\\\"dc82a225-bfff-4bb7-b13b-cdba279a60b7\\\",\\\"userId\\\":\\\"mockUser1\\\",\\\"timestamp\\\":\\\"2022-12-18T21:49+0530\\\",\\\"reference\\\":\\\"mock sample 1\\\",\\\"amount\\\":80}]";
 
@@ -56,7 +40,7 @@ public class AccountTransactionControllerIT
 
         Step 1 — Given — Set up WireMock stub for Get Transactions API
         Step 2 — When — Invoke the Get Transactions API client to fetch a transactions
-        Step 3 — Then — Verify the retrieved transactions
+        Step 3 — Then — Verify the retrieved transactions or Status
         Step 4 - Use verify() method to check if WireMock server received the request for transactions data retrieval
         */
 
@@ -66,13 +50,13 @@ public class AccountTransactionControllerIT
         //Define stub
         log.debug("AccountTransactionControllerIT: wiremock_getTransactions: Attempting stubbing");
         //stubFor(get("http://localhost:" + wireMockRule.port() + APIEndPointsAndConstants.api_getTransactions).willReturn(ok()));
-        stubFor(any(anyUrl()).willReturn(ok()));
+        givenThat((any(anyUrl()).willReturn(ok())));
         log.debug("AccountTransactionControllerIT: wiremock_getTransactions: Stubbing done successfully");
 
         //Invoke the Wiremock stub
        OkHttpClient client = new OkHttpClient().newBuilder().build();
         Request request = new Request.Builder()
-                .url("http://localhost:" + wireMockRule.port() + APIEndPointsAndConstants.api_getTransactions) //hit the end point
+                .url("http://localhost:" + wireMockServer.port() + APIEndPointsAndConstants.api_getTransactions) //hit the end point
                 .method("GET", null)
                 .build();
         log.debug("AccountTransactionControllerIT: wiremock_getTransactions: API Request prepared");
@@ -91,8 +75,8 @@ public class AccountTransactionControllerIT
         log.debug("AccountTransactionControllerIT: wiremock_getTransactions: Asserts executed successfully");
     }
 
-    private void stub_getTransactions() throws JsonProcessingException
-    {
+//    private void stub_getTransactions() throws JsonProcessingException
+//    {
 //        log.debug("AccountTransactionControllerIT: stub_getTransactions: URL " + "http://localhost:8081" + APIEndPointsAndConstants.api_getTransactions);
 //
 //        //Stubbing the "/transactions" end point to return mock transactions
@@ -103,30 +87,25 @@ public class AccountTransactionControllerIT
 //                .willReturn(aResponse().withStatus(200)
 //                        .withBody(mockTransactionJSON)));
 //        log.debug("AccountTransactionControllerIT: stub_getTransactions: /transactions end point stubbed");
-    }
+//    }
 
-    @BeforeEach
-    public void before()
+    @BeforeAll
+    static void setup()
     {
-        wireMockRule = new WireMockServer(8090);
-        configureFor("localhost", 8090 /*WireMockConfiguration.wireMockConfig().dynamicPort().dynamicPort().portNumber()*/);
+        //Setup WireMock server (internally a Jetty server)
+        wireMockServer = new WireMockServer(APIEndPointsAndConstants.wireMockPort);
+        configureFor("localhost", APIEndPointsAndConstants.wireMockPort);
 
-        //wireMockRule = new WireMockRule(WireMockConfiguration.wireMockConfig().dynamicPort());
-        wireMockRule.start();
-        log.debug("AccountTransactionControllerIT: Wiremock started at port " + wireMockRule.port());
+        //Start the WireMock server
+        wireMockServer.start();
+        log.debug("AccountTransactionControllerIT: Wiremock started at port " + wireMockServer.port());
     }
-//
-//    @After
-//    public void after()
-//    {
-//        log.debug("AccountTransactionControllerIT: Wiremock stop being attempted at port " + wireMockRule.port());
-//        wireMockRule.stop();
-//        log.debug("AccountTransactionControllerIT: Wiremock stopped");
-//    }
-//
-//    @AfterEach
-//    public void reset() {
-//        wireMockRule.resetAll();
-//        log.debug("AccountTransactionControllerIT: Wiremock configuration reset");
-//    }
+
+    @AfterAll
+    static void cleanup()
+    {
+        log.debug("AccountTransactionControllerIT: Wiremock stop being attempted at port " + wireMockServer.port());
+        wireMockServer.stop();
+        log.debug("AccountTransactionControllerIT: Wiremock stopped");
+    }
 }
