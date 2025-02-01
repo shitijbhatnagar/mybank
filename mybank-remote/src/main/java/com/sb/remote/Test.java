347bc56506dc8538b0dd59ba7d53d3202df89509
sb <shitijbhatnagar@yahoo.com>
2025-02-01 22:11:30 +0530

    Initial changes for multi-module structure

---
+++ mybank-remote/src/main/java/com/sb/remote/Test.java
@@ -0,0 +1,4 @@
+package com.sb.remote;
+
+public class Test {
+}
--- mybank-runnable/src/main/java/com/sb/mybank/MybankApplication.java
+++ mybank-runnable/src/main/java/com/sb/mybank/MybankApplication.java
@@ -3,9 +3,20 @@ package com.sb.mybank;
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 
 @EnableEncryptableProperties
 @SpringBootApplication
+//@ComponentScan(basePackages={"com.sb.mybank.config", "com.sb.mybank.constants",
+//		"com.sb.mybank.dto",
+//		"com.sb.mybank.exception",
+//		"com.sb.mybank",
+//		"com.sb.mybank.web",
+//		"com.sb.mybank.model",
+//		"com.sb.mybank.repository",
+//		"com.sb.mybank.service",
+//		"com.sb.mybank.util"})
 public class MybankApplication {
 
 	public static void main(String[] args)
